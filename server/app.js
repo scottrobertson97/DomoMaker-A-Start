c183c261f4efd10fecf86d1f65ce5041785b272f
Scott Robertson <scottrobertson97@gmail.com>
2018-10-26 17:00:44 -0400

    done?

--- server/app.js
+++ server/app.js
@@ -7,8 +7,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
-const RedisStore = require('connect-redis');
+const RedisStore = require('connect-redis')(session);
 const url = require('url');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -26,11 +27,11 @@ let redisURL = {
   port: 6379,
 };
 
-let redisPass;
+let redisPASS;
 
 if (process.env.REDISCLOUD_URL) {
   redisURL = url.parse(process.env.REDISCLOUD_URL);
-  redisPass = redisURL.auth.split(':')[1];
+  redisPASS = redisURL.auth.split(':')[1];
 }
 
 const router = require('./router.js');
@@ -40,6 +41,8 @@ const app = express();
 
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
+app.use(cookieParser());
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
@@ -49,16 +52,27 @@ app.use(session({
   store: new RedisStore({
     host: redisURL.hostname,
     port: redisURL.port,
-    pass: redisPass,
+    pass: redisPASS,
   }),
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
-app.use(cookieParser());
+
+app.use(csrf());
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') {
+    return next(err);
+  }
+  console.log('Missing CSRF token');
+  return false;
+});
 
 router(app);
 
